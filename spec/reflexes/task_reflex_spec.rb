80d1fdec5f23cc3c94d23049a95a7a26d8063a0c
Jaime Canales <jaimecanales@Jaimes-MacBook-Air.local>
2022-10-09 17:22:19 -0300

    feat: added rspec and create reflexs tests

---
+++ spec/reflexes/task_reflex_spec.rb
@@ -0,0 +1,60 @@
+require "rails_helper"
+
+RSpec.describe TaskReflex, type: :reflex do
+  let(:reflex) { build_reflex(connection: { current_user: user }) }
+  let(:user) { FactoryBot.create(:user) }
+
+  describe "#toggle" do
+    subject { reflex.run(:toggle) }
+
+    context "element is checked" do
+      let(:task) { FactoryBot.create(:task) }
+
+      before do
+        reflex.element.data_id = task.id
+        reflex.element.checked = true
+      end
+
+      it "completes the task" do
+        expect { subject }.to change { task.reload.complete? }.from(false).to(true)
+      end
+
+      it "broadcasts CableReady operations" do
+        expect { subject }.to broadcast(
+          remove: { selector: "#task_#{task.id}" },
+          insert_adjacent_html: {
+            selector: "#list_#{task.list_id} #complete-tasks",
+            position: "beforeend",
+            html: kind_of(String)
+          },
+          broadcast_to: task.list
+        )
+      end
+    end
+
+    context "element is unchecked" do
+      let(:task) { FactoryBot.create(:task, :complete) }
+
+      before do
+        reflex.element.data_id = task.id
+        reflex.element.checked = false
+      end
+
+      it "incompletes the task" do
+        expect { subject }.to change { task.reload.complete? }.from(true).to(false)
+      end
+
+      it "broadcasts CableReady operations" do
+        expect { subject }.to broadcast(
+          remove: { selector: "#task_#{task.id}" },
+          insert_adjacent_html: {
+            selector: "#list_#{task.list_id} #incomplete-tasks",
+            position: "beforeend",
+            html: kind_of(String)
+          },
+          broadcast_to: task.list
+        )
+      end
+    end
+  end
+end
